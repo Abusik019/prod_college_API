649d3a8b065e20fa93d07a7fa0e193aa0bd1426b
zabit <xaclafun1991@gmail.com>
2024-01-20 19:01:12 +0300

    .

--- college_API/reviews/models.py
+++ college_API/reviews/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from users.models import User
+from data.models import Lecture
+
+
+class Review(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='review_author')
+    lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE, related_name='review')
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, verbose_name='parent', blank=True, null=True)
+
+    text = models.TextField('Сообщение', max_length=5000)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.author} - {self.lecture}'
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
