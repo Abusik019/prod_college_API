0d5199d5a181d3270f296a599fd9981fdccc7e15
zabit <xaclafun1991@gmail.com>
2024-01-24 12:37:17 +0300

    .

--- college_API/config/settings.py
+++ college_API/config/settings.py
@@ -102,6 +102,14 @@ CELERY_BEAT_SCHEDULE = {
 CELERYBEAT_SCHEDULE_FILENAME = 'celerybeat-schedule'
 CELERYBEAT_LOG_LEVEL = 'debug'
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
+
 # ______________________________________________________________________________________________
 
 DATABASES = {
