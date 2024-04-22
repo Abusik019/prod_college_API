1c9a529743df498e705cb97b6df13db5cbc1fdab
Abusik019 <abusroyale@gmail.com>
2024-04-22 17:28:36 +0300

    frontend work

--- frontend/college-api/src/App.jsx
+++ frontend/college-api/src/App.jsx
@@ -1,17 +1,17 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./components/MainPage";
-import ProfilePage from "./components/ProfilePage";
+// import ProfilePage from "./components/ProfilePage";
 
 function App() {
-  return (
-    <BrowserRouter>
-        <Routes>
-          <Route path='*' element={<MainPage />}/>
-          {/* <Route path='profile' element={<ProfilePage />}/> */}
-        </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="*" element={<MainPage />} />
+                {/* <Route path='profile' element={<ProfilePage />}/> */}
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
---
+++ frontend/college-api/src/components/MainPage/Slider/index.jsx
@@ -0,0 +1,36 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './style.css';
+
+import { Keyboard, Navigation, Mousewheel } from 'swiper/modules'; 
+
+export default function Slider() {
+  return (
+    <>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={50}
+        keyboard={true}
+        mousewheel={true}
+        modules={[ Keyboard, Navigation, Mousewheel ]} 
+        className="mySwiper"
+        navigation={true}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
+
