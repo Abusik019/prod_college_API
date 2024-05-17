c05f894bc1acd5330a99df8be08988db16a14d6f
Abusik019 <abusroyale@gmail.com>
2024-05-17 21:13:53 +0300

    fix

--- frontend/college-api/src/components/AboutGroups/index.jsx
+++ frontend/college-api/src/components/AboutGroups/index.jsx
@@ -6,22 +6,33 @@ import getCookie from './../GetCookie/index';
 export const AboutGroups = () => {
     const [btnBgF, setbtnBgF] = useState('focus');
     const [btnBgS, setbtnBgS] = useState('nofocus');
+    const [groups, setGroups] = useState([]);
+    const [isOverflowing, setIsOverflowing] = useState(false);
     const accessToken = getCookie("accessToken");
     const headers = {
         'Authorization': `Bearer ${accessToken}`,
         'Content-Type': 'application/json'
     };
 
-    // useEffect(() => {
-    //     axios.get("http://127.0.0.1:8000/api/v1/data/get_groups", { headers })
-    //         .then((response) => {
-    //             console.log(response);
-    //             // setName(response.data.first_name);
-    //         })
-    //         .catch((error) => {
-    //             console.log(error);
-    //         });
-    // })
+    console.log(groups);
+
+    useEffect(() => {
+        axios.get("http://127.0.0.1:8000/api/v1/data/get_groups", { headers })
+            .then((response) => {
+                // console.log(response);
+                setGroups(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+        const tc = document.querySelector(".groups_main_content");
+            if (tc && tc.childNodes.length > 7) {
+                setIsOverflowing(true);
+            } else {
+                setIsOverflowing(false);
+            }
+    }, []);
 
     return (
         <div className="groups">
@@ -44,6 +55,16 @@ export const AboutGroups = () => {
                 </div>
                 <button className="change_group">Изменить</button>
             </div>
+            <ul className="groups_main_content" style={{ overflowY: isOverflowing ? "scroll" : "auto"   }}>
+                {groups.map(group => (
+                    <li key={group.id}>
+                        <button>+</button>
+                        <h1>{group.facult_name}</h1>
+                        <h2>{group.course_name} курс</h2>
+                        <h3>{group.podgroup_name} группа</h3>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 };
